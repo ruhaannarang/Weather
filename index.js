4128f4ae7e266dd891084f48b9e4ad67abeb290e
Ruhaan Narang <techieruhaan@gmail.com>
2025-07-22 13:16:07 +0530

    used loops

--- index.js
+++ index.js
@@ -25,35 +25,25 @@ async function forecast(){
     let forecastData=await getForecast.json()
     console.log(forecastData.forecast.forecastday[0])
     let forecastTag =document.body.querySelector(".forecast")
-    forecastTag.insertAdjacentHTML("beforeend",`<p>${forecastData.forecast.forecastday[0].date}</p>
+    forecastTag.insertAdjacentHTML("afterbegin",`<p>Date: ${forecastData.forecast.forecastday[0].date}</p>
                                             <p> Max Temperature: ${forecastData.forecast.forecastday[0].day.maxtemp_c}</p>
                                             <p> Min Temperature: ${forecastData.forecast.forecastday[0].day.mintemp_c}</p>
                                             <p> Avg Temperature: ${forecastData.forecast.forecastday[0].day.avgtemp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[0].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[0].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[1].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[1].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[2].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[2].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[3].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[3].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[4].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[4].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[5].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[5].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[6].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[6].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[7].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[7].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[8].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[8].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[9].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[9].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[10].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[10].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[11].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[11].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[12].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[12].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[13].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[13].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[14].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[14].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[15].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[15].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[16].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[16].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[17].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[17].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[18].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[18].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[19].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[19].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[20].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[20].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[21].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[21].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[22].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[22].temp_c}</p>
-                                            <p> ${forecastData.forecast.forecastday[0].hour[23].time.slice(11)}: ${forecastData.forecast.forecastday[0].hour[23].temp_c}</p>
                                             `)
+    let forecastTime =document.body.querySelector(".time")
+    for(let i=0;i<=23;i++){
+      forecastTime.insertAdjacentHTML("beforeend",`<p>${forecastData.forecast.forecastday[0].hour[i].time.slice(11)}</p>`)
+    }
+
+    let forecastColon =document.body.querySelector(".colon")
+    for(let i=0;i<=23;i++){
+      forecastColon.insertAdjacentHTML("beforeend",`<p>:</p>`)
+    }
+
+    let forecastTempC =document.body.querySelector(".tempC")
+    for(let i=0;i<=23;i++){
+      forecastTempC.insertAdjacentHTML("beforeend",`<p>${forecastData.forecast.forecastday[0].hour[i].temp_c}°C</p>`)
+    }
 
 
 }
